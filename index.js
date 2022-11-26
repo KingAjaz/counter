b5c681b6bc2470e318ff63dfecd53a9383d96730
KingAjaz <114471287+KingAjaz@users.noreply.github.com>
2022-11-26 17:52:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,3 +29,43 @@ function save(){
     counted = 0
     countor.textContent = counted;
 }
+
+let homeScore = document.getElementById("home-el")
+let guestScore = document.getElementById("guest-el")
+console.log(homeScore)
+console.log(guestScore)
+let homePlus = 0
+let guestPlus= 0
+
+function plus1(){
+    homePlus += 1
+    homeScore.textContent = homePlus
+}
+function plus2(){
+    homePlus += 2
+    homeScore.textContent = homePlus
+}
+function plus3(){
+    homePlus += 3
+    homeScore.textContent = homePlus
+}
+
+function plus11(){
+    guestPlus += 1
+    guestScore.textContent = guestPlus
+}
+function plus22(){
+    guestPlus += 2
+    guestScore.textContent = guestPlus
+}
+function plus33(){
+    guestPlus += 3
+    guestScore.textContent = guestPlus
+}
+
+function reset(){
+    homeScore.textContent = 0
+    guestScore.textContent = 0
+    homePlus = 0
+    guestPlus = 0
+}
